dfd48027b75d8cffb57111f3a6928457d30d9835
Dmytro Savych <92891294+Sava12012@users.noreply.github.com>
2023-02-12 20:23:50 +0200

    Initial commit

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  margin: 30px auto;
+  padding: 20px;
+  max-width: 600px;
+  text-align: center;
+  box-shadow: var(--main-shadow);
+  border-radius: 10px;
+  background-color: var(--box);
+`;
+
+export const Title = styled.h1`
+  margin-bottom: 20px;
+  padding: 5px;
+  font-size: 44px;
+  color: var(--primary);
+
+  box-shadow: var(--main-shadow);
+  border-radius: 10px;
+  background-color: var(--box-statistics);
+`;
+
+export const Subtitle = styled.h2`
+  margin-bottom: 20px;
+  padding: 5px;
+  font-size: 32px;
+  color: var(--primary);
+  box-shadow: var(--main-shadow);
+  border-radius: 10px;
+  background-color: var(--box-statistics);
+`;
